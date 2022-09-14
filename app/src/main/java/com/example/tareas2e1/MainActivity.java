563fac65d667246f8c3b6832d9c05937f1d03e8c
josueprogram2003 <josuedominguezonline12@gmail.com>
2022-09-14 10:29:06 -0500

    Tarea JDOMINGUREZ DAMS2E1

---
+++ app/src/main/java/com/example/tareas2e1/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.tareas2e1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.tareas2e1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.icon_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.icon_user:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.icon_settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+}
